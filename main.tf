e548ca48754574ac1d0246932810e6fb3caf29c5
Maxime Roy <maxime.roy@fxinnovation.com>
2020-02-27 15:33:23 -0500

    fix: Added if statement to direct refs to vnet

--- main.tf
+++ main.tf
@@ -29,16 +29,16 @@ resource "azurerm_virtual_network" "this" {
 module "subnets" {
   source               = "./modules/subnets/"
   enabled              = var.enabled
-  resource_group_name  = azurerm_virtual_network.this[0].resource_group_name
-  virtual_network_name = azurerm_virtual_network.this[0].name
+  resource_group_name  = var.enabled ? azurerm_virtual_network.this[0].resource_group_name : ""
+  virtual_network_name = var.enabled ? azurerm_virtual_network.this[0].name : ""
   subnets_config       = var.subnets_config
 }
 
 module "network_security_groups" {
   source                         = "./modules/network-security-groups/"
   enabled                        = var.enable_nsg
-  resource_group_name            = azurerm_virtual_network.this[0].resource_group_name
-  location                       = azurerm_virtual_network.this[0].location
+  resource_group_name            = var.enabled ? azurerm_virtual_network.this[0].resource_group_name : ""
+  location                       = var.enabled ? azurerm_virtual_network.this[0].location : ""
   subnets_config                 = var.subnets_config
   subnets_ids_map                = module.subnets.subnets_ids_map
   network_security_groups_config = var.network_security_groups_config
@@ -52,8 +52,8 @@ module "network_security_groups" {
 
 module "route_tables" {
   source              = "./modules/route-tables/"
-  resource_group_name = azurerm_virtual_network.this[0].resource_group_name
-  location            = azurerm_virtual_network.this[0].location
+  resource_group_name = var.enabled ? azurerm_virtual_network.this[0].resource_group_name : ""
+  location            = var.enabled ? azurerm_virtual_network.this[0].location : ""
   subnets_config      = var.subnets_config
   subnets_ids_map     = module.subnets.subnets_ids_map
   route_tables_config = var.route_tables_config
